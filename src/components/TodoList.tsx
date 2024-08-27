f83d7d85485bee57d3a7bb36f1f6c55e6f4953b8
Muhammad Nouman <mnomay07@gmail.com>
2024-08-27 15:45:51 +0500

    Updates suggested to sort based on title

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../app/store';
 import { removeTodo } from '../features/todo/todoSlice';
@@ -7,9 +7,15 @@ const TodoList: React.FC = () => {
   const todos = useSelector((state: RootState) => state.todos.todos);
   const dispatch = useDispatch();
 
+  const sortedTodos = useMemo(() => {
+    return todos.concat().sort((a, b) => {
+      return a.title.localeCompare(b.title);
+    });
+  }, [todos]);
+
   return (
     <ul className="list-group">
-      {todos.map((todo) => (
+      {sortedTodos.map((todo) => (
         <li key={todo.id} className="list-group-item d-flex justify-content-between align-items-center">
           {todo.title}
           <button onClick={() => dispatch(removeTodo(todo.id))} className="btn btn-danger btn-sm">
